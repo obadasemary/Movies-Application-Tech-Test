27078bd9115ca7fae953f2326d0d427734b9e3a8
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-11 03:15:20 +0300

    implement URLComponents extension

---
+++ Movies Application/Extensions/URLComponents.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © Modus Create. All rights reserved.
+//
+
+import Foundation
+
+extension URLComponents {
+    
+    mutating func setQueryItems(with parameters: [String: String]) {
+        self.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+    }
+}
