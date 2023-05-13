cd83e76fa4d15134b53bb595453165bf479c1375
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-13 13:26:37 +0300

    delete movies client

--- Movies Application/Client/MoviesClient.swift
+++ Movies Application/Client/MoviesClient.swift
@@ -4,24 +4,24 @@
 
 import Foundation
 
-public class MoviesClient {
-	let apiKey: String
-	let baseUrl: URL
-	
-	public init(apiKey: String) {
-		self.apiKey = apiKey
-		
-		guard let url = URL(string: "https://api.themoviedb.org/3/") else {
-			preconditionFailure("Unable to build URL")
-		}
-		self.baseUrl = url
-	}
-	
-	public func popularMovies() async throws -> [MovieSummary] {
-		fatalError("Missing Implementation")
-	}
-	
-	public func movieDetails(_ id: Int) async throws -> MovieDetails {
-		fatalError("Missing Implementation")
-	}
-}
+//public class MoviesClient {
+//	let apiKey: String
+//	let baseUrl: URL
+//
+//	public init(apiKey: String) {
+//		self.apiKey = apiKey
+//
+//		guard let url = URL(string: "https://api.themoviedb.org/3/") else {
+//			preconditionFailure("Unable to build URL")
+//		}
+//		self.baseUrl = url
+//	}
+//
+//	public func popularMovies() async throws -> [MovieSummary] {
+//		fatalError("Missing Implementation")
+//	}
+//
+//	public func movieDetails(_ id: Int) async throws -> MovieDetails {
+//		fatalError("Missing Implementation")
+//	}
+//}
